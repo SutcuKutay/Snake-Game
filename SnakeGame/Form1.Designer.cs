42a8271e5bbbf689a6c7560cd287df641acf0c61
Kutay Sütçü <kutay.sutcu@gmail.com>
2023-10-12 19:10:03 +0300

    Game Files Uploaded

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxSnake = new System.Windows.Forms.PictureBox();
+            this.pictureBoxApple = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSnake)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxApple)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxSnake
+            // 
+            this.pictureBoxSnake.Image = global::SnakeGame.Properties.Resources.wide_snake_diet_1b7336d3f7de41f98827c804ca465084;
+            this.pictureBoxSnake.Location = new System.Drawing.Point(59, 74);
+            this.pictureBoxSnake.Name = "pictureBoxSnake";
+            this.pictureBoxSnake.Size = new System.Drawing.Size(100, 80);
+            this.pictureBoxSnake.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSnake.TabIndex = 0;
+            this.pictureBoxSnake.TabStop = false;
+            // 
+            // pictureBoxApple
+            // 
+            this.pictureBoxApple.Image = global::SnakeGame.Properties.Resources.istockphoto_184276818_612x612;
+            this.pictureBoxApple.Location = new System.Drawing.Point(566, 101);
+            this.pictureBoxApple.Name = "pictureBoxApple";
+            this.pictureBoxApple.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxApple.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxApple.TabIndex = 1;
+            this.pictureBoxApple.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.White;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonStart.Location = new System.Drawing.Point(273, 373);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(244, 65);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBoxApple);
+            this.Controls.Add(this.pictureBoxSnake);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSnake)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxApple)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxSnake;
+        private System.Windows.Forms.PictureBox pictureBoxApple;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyUp += Form1_KeyUp;
+        }
+
+        string direction;
+        int snakeHeight, snakeWidth, appleHeight, appleWidth;
+        PictureBox snake, apple;
+        Random rnd = new Random();
+        
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            buttonStart.Visible = false;
+            buttonStart.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            snake = pictureBoxSnake;
+            snakeHeight = snake.Height;
+            snakeWidth = snake.Width;
+
+            apple = pictureBoxApple;
+            appleHeight = apple.Height;
+            appleWidth = apple.Width;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Snake's movement.
+            if (direction == "up")
+            {
+                pictureBoxSnake.Top -= 3; //Speed of the snake
+            }
+            else if (direction == "down")
+            {
+                pictureBoxSnake.Top += 3;
+            }
+            else if (direction == "left")
+            {
+                pictureBoxSnake.Left -= 3;
+            }
+            else if (direction == "right")
+            {
+                pictureBoxSnake.Left += 3;
+            }
+
+            //When Snake reaches the Apple.
+            if(snake.Bounds.IntersectsWith(apple.Bounds))
+            {
+                eat();
+            }
+        }
+
+        private void eat()
+        {
+            apple.Location = new Point(rnd.Next(1,500), rnd.Next(1,400));
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                direction = "up";
+            }else if (e.KeyCode == Keys.Down)
+            {
+                direction = "down";
+            }else if (e.KeyCode == Keys.Left)
+            {
+                direction = "left";
+            }else if (e.KeyCode == Keys.Right)
+            {
+                direction = "right";
+            }else if (e.KeyCode == Keys.Escape)
+            {
+                timer.Stop();
+                buttonStart.Visible = true;
+                buttonStart.Enabled = true;
+            }
+        }
+    }
+}
